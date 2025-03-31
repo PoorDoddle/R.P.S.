c5b9d34dfd2ee19d96377748cf0aca801ca62d73
PoorDoddle <bartu_bady@hotmail.com>
2025-03-31 11:28:00 +0300

    fixed the bug which led to 6 user inputs
    1 input was for nothing so it was bad user experience

--- scripts.js
+++ scripts.js
@@ -7,60 +7,51 @@ function getHumanChoice() {
   let humanChoice = prompt("enter your choice");
   return humanChoice.toLowerCase();
 }
-let humanScore = 0;
-let computerScore = 0;
+
 function playRound(human, computer) {
   if (human == "rock" && computer == 0) {
     console.log("tie");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "rock" && computer == 1) {
     computerScore++;
     console.log("you lose");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "rock" && computer == 2) {
     humanScore++;
     console.log("you win");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "scissors" && computer == 0) {
     computerScore++;
     console.log("you lose");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "scissors" && computer == 1) {
     humanScore++;
     console.log("you win");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "scissors" && computer == 0) {
     console.log("tie");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "paper" && computer == 0) {
     humanScore++;
     console.log("you win");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "paper" && computer == 1) {
     console.log("tie");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   } else if (human == "paper" && computer == 2) {
     computerScore++;
     console.log("you lose");
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
   }
 }
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
 function playGame() {
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
+  humanSelection = getHumanChoice();
+  computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
   return console.log(`human: ${humanScore}, computer: ${computerScore}`);
 }
